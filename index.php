e35eddd3709ed4e47bcfbe629bb6db423be32ef6
Brice AIELLO <brice.aiello@fuwuzone.com>
2011-10-09 23:50:01 +0200

    improvements

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
 <head>
 	<meta charset="utf-8"/>
 	<title>FuwuZone</title>
-	<link rel="stylesheet" href="https://raw.github.com/briceaiello/FuwuZone/master/style/original/base.css">
+	<link rel="stylesheet" href="style/original/base.css">
 	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
-	<script type="text/javascript" src="https://raw.github.com/briceaiello/FuwuZone/master/scripts/ob.js"></script>
+	<script type="text/javascript" src="scripts/ob.js"></script>
 </head>
 
 <body>
@@ -14,7 +14,7 @@
 	<div id="h">
 		<div id="hp">left</div>
 		<div id="hu">
-			<span id="huc" href="#" rel="ob">
+			<span id="huc" href="http://files.formspring.me/profile/20110331/n4d94c440bb823.jpg" rel="obconnect">
 				<span>Sign in</span>
 			</span>
 		</div>
--- scripts/ob.js
+++ scripts/ob.js
@@ -18,17 +18,44 @@ $(document).ready(function(){
 ob = {
 	
 	init : function(){
-		$("*[rel='ob']").click(function(){
+		$("*[rel='obconnect']").click(function(){
 			ob.thing = $(this).attr("href");
-			ob.open(ob.thing);
+			ob.start(ob.thing);
 			return false;
 		});
 	},
-	open : function(thing){
+	start : function(thing){
 		ob.thing = thing;
 		$("body").append('<div id="ob"><div id="oba"></div><div id="obl"></div><div id="obc"><div id="obcr"><div id="obcc"></div></div></div></div>');
 		$("#oba").css("display","block").fadeTo(500,0.8);
 		$("#obl").css("display","block").fadeTo(500,1);
+		ob.obj = new Object();
+		ob.obj.src = ob.thing;
+		ob.timer = window.setInterval(ob.load,100);
+		$("#oba").click(ob.close);
+	},
+	load : function(){
+		if(ob.obj.complete){
+			window.clearInterval(ob.timer);
+			window.open();
+		};
+	},
+	open : function(){
+		$("#obl").fadeOut(300,function(){
+			$("#obl").css("display","none");
+			ob.width = ob.obj.width;
+			ob.height = ob.obj.height;
+			$("#obcc").append(ob.thing);
+			$("#obcc").animate({width:ob.width,height:ob.height},500,"linear",function(){
+				$("#obc").css("display","block");
+			});
+		});
+	},
+	close : function(){
+		$("#obc,#obl,#oba").fadeOut(500,function(){
+			$("#obc,#obl,#oba").css("display","none");
+			$("#ob").remove();
+		});
 	}
 
 }
