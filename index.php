43465b9c97a15a2757a7165c68ce68504d21e2e0
Brice AIELLO <brice.aiello@fuwuzone.com>
2011-10-09 18:12:47 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8"/>
+	<title>FuwuZone</title>
+	<link rel="stylesheet" href="style/original/base.css">
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+	<script type="text/javascript" src="./scripts/ob.js"></script>
+</head>
+
+<body>
+
+	<div id="h">
+		<div id="hp">left</div>
+		<div id="hu">
+			<span id="huc">
+				<span href="#" rel="ob">Sign in</span>
+			</span>
+		</div>
+	</div>
+	
+</body>
+
+</html>
